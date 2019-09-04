32432e741c8ed5cb037bda6ff13a9526af19a0fb
pablo222017 <pablo222005@gmail.com>
2019-09-04 00:55:33 -0300

    TP01

--- TP01/TP01/main.c
+++ TP01/TP01/main.c
@@ -6,109 +6,59 @@
 int main()
 {
     float num1,num2,resultado;
-    char seguir,opcion;
-    do
-    {
-        fflush(stdin);
-        scanf("%c",&seguir);
-        printf("\nIngrese primer numero: ");
-        scanf("%f",&num1);
-        printf("\nIngrese segundo numero: ");
-        scanf("%f",&num2);
-        menu();
-        printf("\nElija una opcion: ");
-        fflush(stdin);
-        opcion=getchar();
-        switch(opcion)
+    //int contOpcion=0;
+    char opcion;
+    printf("      ¡¡¡ Bienvenido a su calculadora !!! \n");
+    printf("                                         \n");
+    printf("Para comenzar, ingrese primero los operandos.\n");
+    printf("\nIngrese primer numero: ");
+    scanf("%f",&num1);
+    printf("\nIngrese segundo numero: ");
+    scanf("%f",&num2);
+    system("cls");
+    menu();
+    while(opcion!='6')
         {
-        case '1':
-        resultado=sumar(num1,num2);
-        printf("\nEl resultado de %.2f + %.2f es: %.2f\n",num1,num2,resultado);
-        system("pause");
-        break;
-
-        case '2':
-        resultado=restar(num1,num2);
-        printf("\nEl resultado de %.2f - %.2f es: %.2f\n",num1,num2,resultado);
-        system("pause");
-        break;
-
-        case '3':
-        resultado=multiplicar(num1,num2);
-        printf("\nEl resultado de %.2f x %.2f es: %.2f\n",num1,num2,resultado);
-        system("pause");
-        break;
-
-        case '4':
-        if(num1<1)
+            printf("\nIngrese la opcion correspondiente a la operacion deseada: ");
+            fflush(stdin);
+            opcion=getchar();
+            switch(opcion)
             {
-                printf("No es posible realizar divisiones por 0, elija otro numero");
-                scanf("%f",&num1);
-            }else if
-            {
-
+            case '1':
+            resultado=sumar(num1,num2);
+            printf("\nEl resultado de %.2f + %.2f es: %.2f\n",num1,num2,resultado);
+            //system("pause");
+            break;
+            case '2':
+            resultado=restar(num1,num2);
+            printf("\nEl resultado de %.2f - %.2f es: %.2f\n",num1,num2,resultado);
+            //system("pause");
+            break;
+            case '3':
+            resultado=multiplicar(num1,num2);
+            printf("\nEl resultado de %.2f x %.2f es: %.2f\n",num1,num2,resultado);
+            //system("pause");
+            break;
+            case '4':
+            if(num1<1 || num2<0)
+                {
+                    printf("\nNo es posible realizar divisiones por 0, elija otro numero: ");
+                    scanf("%f",&num1);
+                    printf("\nNo es posible realizar divisiones por 0, elija otro numero: ");
+                    scanf("%f",&num2);
+                }
+            resultado=dividir(num1,num2);
+            printf("\nEl resultado de %.2f / %.2f es: %.2f\n",num1,num2,resultado);
+            //system("pause");
+            break;
+            case '5':
+            printf("\nUsted eligio salir\n");
+            //system("pause");
+            break;
+            default:
+            printf("\nUsted eligio salir\n");
+            break;
             }
-        resultado=dividir(num1,num2);
-        printf("\nEl resultado de %.2f / %.2f es: %.2f\n",num1,num2,resultado);
-        break;
-
-        case '5':
-        printf("\nUsted eligio salir\n");
-        system("pause");
-        break;
-
-        default:
-        printf("\nOpcion invalida\n\n");
-        system("pause");
-        break;
         }
-    }
-    while(seguir=='s');
     return 0;
 }
-/*
-do{
-    menu();
-    switch(menu())
-    {
-        case 'a':
-        resultado=sumar(num1,num2);
-        printf("\nEl resultado de %.2f + %.2f es: %.2f\n",num1,num2,resultado);
-        system("pause");
-        break;
-
-        case 'b':
-        resultado=restar(num1,num2);
-        printf("\nEl resultado de %.2f - %.2f es: %.2f\n",num1,num2,resultado);
-        system("pause");
-        break;
-
-        case 'c':
-        resultado=multiplicar(num1,num2);
-        printf("\nEl resultado de %.2f x %.2f es: %.2f\n",num1,num2,resultado);
-        system("pause");
-        break;
-
-        case 'd':
-        printf("\nUsted eligio dividir\n");
-        system("pause");
-        break;
-
-        case 'e':
-        printf("\nUsted eligio salir\n");
-        seguir = 'n';
-        system("pause");
-        break;
-
-        default:
-        printf("\nOpcion invalida\n\n");
-        system("pause");
-        break;
-        }
-    printf("\nDesea continuar operando? (s/n) \n");
-    seguir=getchar();
-    }while(seguir == 's');
-
-
-
-*/
